62490e53590183465ff7a9691127a665102c1a60
nickrally <nickrally2009@gmail.com>
2022-10-21 02:26:40 -0600

    migration work for existing db

--- services/flaskapp/project/__init__.py
+++ services/flaskapp/project/__init__.py
@@ -1,5 +1,5 @@
 from flask import Flask
-from .extensions import db
+from .extensions import db, migrate
 from .routes.main import main
 
 
@@ -7,5 +7,6 @@ def create_app():
     app = Flask(__name__)
     app.config.from_object("project.config.Config")
     db.init_app(app)
+    migrate.init_app(app, db)
     app.register_blueprint(main)
     return app
--- services/flaskapp/project/models/thing.py
+++ services/flaskapp/project/models/thing.py
@@ -4,3 +4,4 @@ from ..extensions import db
 class Thing(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(50))
+    description = db.Column(db.String(255))
